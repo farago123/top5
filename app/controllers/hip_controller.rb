c6fb2e717da680e6bcece59cfc0d5dacc826ae6d
farago <faragopeter8@gmail.com>
2015-09-03 22:58:05 +0000

    top5 homework, general cleanup

--- app/controllers/hip_controller.rb
+++ app/controllers/hip_controller.rb
@@ -1,5 +1,9 @@
 class HipController < ApplicationController
-    def hop
+    def topSongs
         @songs = Song.order(rating: :desc).limit(5)
+    end
+    
+    def topArtists
+        @artists = Artist.order(rating: :desc).limit(5)
     end
 end
